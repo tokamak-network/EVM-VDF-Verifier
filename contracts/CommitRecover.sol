19e56fdb6daefdb1449b6d34fb901b24e5925041
지의신 <64007655+usgeeus@users.noreply.github.com>
2023-11-24 10:38:58 +0900

    apply BigNumber Funcs to CommitRecover

--- contracts/CommitRecover.sol
+++ contracts/CommitRecover.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
-
 import "./libraries/Pietrzak_VDF.sol";
+
+//import "hardhat/console.sol";
 
 /**
  * @title Bicorn-RX Commit-Reveal-Recover
@@ -19,25 +20,27 @@ contract CommitRecover {
      * @notice Stages of the contract
      * @notice Recover can be performed in the Reveal and Finished stages.
      */
+    using BigNumbers for *;
+    using Pietrzak_VDF for *;
     enum Stages {
         Commit,
         Reveal,
         Finished
     }
     struct ValueAtRound {
-        uint256 omega; // the random number
+        BigNumber omega; // the random number
         bytes bStar; //hash of commitsString
         uint256 numOfParticipants; // number of participants
-        bytes g; // a value generated from the generator list
-        bytes h; // a value generated from the VDF(g)
-        bytes n; // modulor
+        BigNumber g; // a value generated from the generator list
+        BigNumber h; // a value generated from the VDF(g)
+        BigNumber n; // modulor
         uint256 T;
         bool isCompleted; // omega is finalized when this is true
         bool isAllRevealed; // true when all participants have revealed
     }
     struct CommitRevealValue {
-        bytes c;
-        bytes a;
+        BigNumber c;
+        BigNumber a;
         address participantAddress;
     }
     struct UserAtRound {
@@ -52,7 +55,7 @@ contract CommitRecover {
     uint256 public commitRevealDuration; //commit + reveal period, commitRevealDuration - commitDuration => revealDuration
     uint256 public count; //This variable is used to keep track of the number of commitments and reveals, and to check if anything has been committed when moving to the reveal stage.
     uint256 public round; //first round is 1, second round is 2, ...
-    string public commitsString; //concatenated string of commits
+    bytes public commitsString; //concatenated string of commits
     Stages public stage;
     //bool public isHAndBStarSet;
     mapping(uint256 round => mapping(uint256 index => CommitRevealValue)) public commitRevealValues; //
@@ -62,16 +65,21 @@ contract CommitRecover {
     /* Events */
     event CommitC(
         address participant,
-        uint256 commit,
-        string commitsString,
+        BigNumber commit,
+        bytes commitsString,
         uint256 commitCount,
         uint256 commitTimestamp
     );
-    event RevealA(address participant, bytes a, uint256 revealLeftCount, uint256 revealTimestamp);
+    event RevealA(
+        address participant,
+        BigNumber a,
+        uint256 revealLeftCount,
+        uint256 revealTimestamp
+    );
     event Recovered(
         address msgSender,
-        uint256 recov,
-        uint256 omegaRecov,
+        BigNumber recov,
+        BigNumber omegaRecov,
         uint256 recoveredTimestamp
     );
     event Start(
@@ -79,15 +87,15 @@ contract CommitRecover {
         uint256 startTime,
         uint256 commitDuration,
         uint256 commitRevealDuration,
-        bytes n,
-        bytes g,
-        bytes h,
+        BigNumber n,
+        BigNumber g,
+        BigNumber h,
         uint256 T,
         uint256 round
     );
     event CalculatedOmega(
         uint256 round,
-        uint256 omega,
+        BigNumber omega,
         uint256 calculatedTimestamp,
         bool isCompleted
     );
@@ -117,14 +125,21 @@ contract CommitRecover {
      * @notice if count == 0, update valuesAtRound, stage
      * @notice update userInfosAtRound
      */
-    function reveal(bytes calldata _a) public {
+
+    function reveal(BigNumber calldata _a) public {
         uint256 _round = round;
         UserAtRound memory _user = userInfosAtRound[msg.sender][_round];
         require(_user.committed, "NotCommittedParticipant");
         require(!_user.revealed, "AlreadyRevealed");
+        // console.log("reveal contract-----!");
+        // console.logBytes(commitRevealValues[_round][_user.index].c.val);
+        // console.logBytes(valuesAtRound[_round].g.modexp(_a, valuesAtRound[_round].n).val);
+        // console.log(commitRevealValues[_round][_user.index].c.bitlen);
+        // console.log(valuesAtRound[_round].g.modexp(_a, valuesAtRound[_round].n).bitlen);
+        // console.log(commitRevealValues[_round][_user.index].c.neg);
+        // console.log(valuesAtRound[_round].g.modexp(_a, valuesAtRound[_round].n).neg);
         require(
-            Pietrzak_VDF.equal(
-                Pietrzak_VDF.powerModN(valuesAtRound[_round].g, _a, valuesAtRound[_round].n),
+            (valuesAtRound[_round].g.modexp(_a, valuesAtRound[_round].n)).eq(
                 commitRevealValues[_round][_user.index].c
             ),
             "ANotMatchCommit"
@@ -141,33 +156,27 @@ contract CommitRecover {
         emit RevealA(msg.sender, _a, _count, block.timestamp);
     }
 
-    function calculateOmega() public returns (uint256) {
+    function calculateOmega() public returns (BigNumber memory) {
         uint256 _round = round;
         require(valuesAtRound[_round].isAllRevealed, "NotAllRevealed");
         require(!valuesAtRound[_round].isCompleted, "OmegaAlreadyCompleted");
         checkStage();
         equalStage(Stages.Finished);
         uint256 _numOfParticipants = valuesAtRound[_round].numOfParticipants;
-        uint256 _omega = 1;
+        BigNumber memory _omega = BigNumbers.one();
         bytes memory _bStar = valuesAtRound[_round].bStar;
-        bytes memory _h = valuesAtRound[round].h;
-        bytes memory _n = valuesAtRound[round].n;
+        BigNumber memory _h = valuesAtRound[round].h;
+        BigNumber memory _n = valuesAtRound[round].n;
         bool _isCompleted = true;
         for (uint256 i = 0; i < _numOfParticipants; i++) {
-            _omega = mulmod(
-                _omega,
-                Pietrzak_VDF.powerModN(
-                    Pietrzak_VDF.powerModN(
-                        _h,
-                        Pietrzak_VDF.modHash(
-                            _n,
-                            bytes.concat(commitRevealValues[_round][i].c, _bStar)
-                        ),
+            _omega = _omega.modmul(
+                _h
+                    .modexp(
+                        // Pietrzak_VDF.modHash(_n, bytes.concat(commitRevealValues[_round][i].c, _bStar)),
+                        _n.modHash(bytes.concat(commitRevealValues[_round][i].c.val, _bStar)),
                         _n
-                    ),
-                    commitRevealValues[_round][i].a,
-                    _n
-                ),
+                    )
+                    .modexp(commitRevealValues[_round][i].a, _n),
                 _n
             );
         }
@@ -187,8 +196,8 @@ contract CommitRecover {
      * @notice calculate and finalize omega
      */
     function recover(uint256 _round, Pietrzak_VDF.VDFClaim[] calldata proofs) public {
-        uint256 recov = 1;
-        bytes memory _n = valuesAtRound[_round].n;
+        BigNumber memory recov = BigNumbers.one();
+        BigNumber memory _n = valuesAtRound[_round].n;
         //require(stage != Stages.Commit, "FunctionInvalidAtThisStage");
         checkStage();
         overStage(Stages.Commit);
@@ -197,15 +206,13 @@ contract CommitRecover {
         require(valuesAtRound[_round].T == proofs[0].T, "TNotMatched");
         Pietrzak_VDF.verifyRecursiveHalvingProof(proofs);
         for (uint256 i = 0; i < valuesAtRound[_round].numOfParticipants; i++) {
-            bytes memory _c = commitRevealValues[_round][i].c;
-            bytes memory temp = Pietrzak_VDF.powerModN(
-                _c,
-                Pietrzak_VDF.modHash(_n, bytes.concat(_c, _bStar)),
-                _n
-            );
-            recov = mulmod(recov, temp, _n);
+            BigNumber memory _c = commitRevealValues[_round][i].c;
+            BigNumber memory temp = _c.modexp(_n.modHash(bytes.concat(_c.val, _bStar)), _n);
+            //recov = mulmod(recov, temp, _n);
+            recov = recov.modmul(temp, _n);
         }
-        require(recov == proofs[0].x, "RecovNotMatchX");
+        //require(recov == proofs[0].x, "RecovNotMatchX");
+        require(recov.eq(proofs[0].x), "RecovNotMatchX");
         valuesAtRound[_round].isCompleted = true;
         valuesAtRound[_round].omega = proofs[0].y;
         stage = Stages.Finished;
@@ -224,7 +231,7 @@ contract CommitRecover {
     function start(
         uint256 _commitDuration,
         uint256 _commitRevealDuration,
-        bytes calldata _n,
+        BigNumber calldata _n,
         Pietrzak_VDF.VDFClaim[] calldata _proofs
     ) public {
         //require(_proofs[0].x < _n, "GreaterOrEqualThanN");
@@ -234,7 +241,7 @@ contract CommitRecover {
             "CommitRevealDurationLessThanCommitDuration"
         );
         require(stage == Stages.Finished, "StageNotFinished");
-        Pietrzak_VDF.verifyRecursiveHalvingProof(_proofs);
+        //require(Pietrzak_VDF.verifyRecursiveHalvingProof(_proofs), "not verified");
         round += 1;
         stage = Stages.Commit;
         startTime = block.timestamp;
@@ -271,8 +278,9 @@ contract CommitRecover {
             if (count != 0) {
                 nextStage();
                 valuesAtRound[round].numOfParticipants = count;
-                uint256 _bStar = uint256(keccak256(abi.encodePacked(commitsString))) %
-                    valuesAtRound[round].n;
+                // uint256 _bStar = uint256(keccak256(abi.encodePacked(commitsString))) %
+                //     valuesAtRound[round].n;
+                bytes memory _bStar = valuesAtRound[round].n.modHash(bytes(commitsString)).val;
                 valuesAtRound[round].bStar = _bStar;
             } else {
                 stage = Stages.Finished;
@@ -293,15 +301,19 @@ contract CommitRecover {
      * @notice The participant can only commit once
      * @notice check period, update stage if needed, revert if not currently at commit stage
      */
-    function commit(uint256 _commit) public {
+    function commit(BigNumber memory _commit) public {
         require(!userInfosAtRound[msg.sender][round].committed, "AlreadyCommitted");
         checkStage();
         equalStage(Stages.Commit);
         uint256 _count = count;
-        string memory _commitsString = commitsString;
-        _commitsString = string.concat(_commitsString, Pietrzak_VDF.toString(_commit));
+        bytes memory _commitsString = commitsString;
+        _commitsString = bytes.concat(_commitsString, _commit.val);
         userInfosAtRound[msg.sender][round] = UserAtRound(_count, true, false);
-        commitRevealValues[round][_count] = CommitRevealValue(_commit, 0, msg.sender); //index starts from 0, so _count -1
+        commitRevealValues[round][_count] = CommitRevealValue(
+            _commit,
+            BigNumbers.one(),
+            msg.sender
+        ); //index starts from 0, so _count -1
         commitsString = _commitsString;
         count = ++_count;
         emit CommitC(msg.sender, _commit, _commitsString, _count, block.timestamp);
--- contracts/libraries/Pietrzak_VDF.sol
+++ contracts/libraries/Pietrzak_VDF.sol
@@ -1,66 +1,66 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.19;
+import "./BigNumbers.sol";
 
 library Pietrzak_VDF {
     bytes16 private constant _SYMBOLS = "0123456789abcdef";
-    //abi.encodePacked(uint256(1))
-    bytes private constant _ONE = abi.encodePacked(uint256(1));
-    bytes private constant _TWO = abi.encodePacked(uint256(2));
-
-    struct BigNumber {
-        bytes val;
-        uint256 bitlen;
-    }
+    using BigNumbers for *;
 
     struct VDFClaim {
         uint256 T;
-        bytes n;
-        bytes x;
-        bytes y;
-        bytes v;
+        BigNumber n;
+        BigNumber x;
+        BigNumber y;
+        BigNumber v;
     }
 
     struct SingHalvProofOutput {
         bool verified;
         bool calculated;
-        bytes x_prime;
-        bytes y_prime;
+        BigNumber x_prime;
+        BigNumber y_prime;
         uint256 T_half;
     }
 
-    function modHash(bytes memory n, bytes memory _strings) internal view returns (bytes memory) {
+    function modHash(
+        BigNumber memory n,
+        bytes memory _strings
+    ) internal view returns (BigNumber memory) {
         //return uint256(keccak256(abi.encodePacked(strings))) % n;
-        return powerModN(abi.encodePacked(keccak256(_strings)), _ONE, n);
+        //return powerModN(abi.encodePacked(keccak256(_strings)), _one, n);
+        return abi.encodePacked(keccak256(_strings)).init(false).mod(n);
     }
 
     function processSingleHalvingProof(
         VDFClaim calldata vdfClaim
     ) internal view returns (SingHalvProofOutput memory) {
+        BigNumber memory _zero = BigNumbers.zero();
+        BigNumber memory _two = BigNumbers.two();
         if (vdfClaim.T == 1) {
             //if (vdfClaim.y == powerModN(vdfClaim.x, 2, vdfClaim.n)) {
-            if (equal(vdfClaim.y, powerModN(vdfClaim.x, vdfClaim.v, vdfClaim.n))) {
-                return SingHalvProofOutput(true, false, "", "", 0);
+            //if (equal(vdfClaim.y, powerModN(vdfClaim.x, vdfClaim.v, vdfClaim.n))) {
+            if (vdfClaim.y.eq(vdfClaim.x.modexp(vdfClaim.v, vdfClaim.n))) {
+                return SingHalvProofOutput(true, false, _zero, _zero, 0);
             } else {
-                return SingHalvProofOutput(false, false, "", "", 0);
+                return SingHalvProofOutput(false, false, _zero, _zero, 0);
             }
         } else {
             uint256 tHalf;
-            bytes memory y = vdfClaim.y;
-            bytes memory r = modHash(vdfClaim.x, bytes.concat(vdfClaim.y, vdfClaim.v));
+            BigNumber memory y = vdfClaim.y;
+            BigNumber memory r = modHash(vdfClaim.x, bytes.concat(vdfClaim.y.val, vdfClaim.v.val));
 
             if (vdfClaim.T & 1 == 0) {
                 tHalf = vdfClaim.T / 2;
             } else {
                 tHalf = (vdfClaim.T + 1) / 2;
-                y = powerModN(y, _TWO, vdfClaim.n);
+                y = y.modexp(_two, vdfClaim.n);
             }
             return
                 SingHalvProofOutput(
                     true,
                     true,
-                    powerModN(powerModN(vdfClaim.x, r, vdfClaim.n), vdfClaim.v, vdfClaim.n),
-                    powerModN(powerModN(vdfClaim.v, r, vdfClaim.n), y, vdfClaim.n),
+                    vdfClaim.x.modexp(r, vdfClaim.n).modexp(vdfClaim.v, vdfClaim.n),
+                    vdfClaim.v.modexp(r, vdfClaim.n).modexp(y, vdfClaim.n),
                     tHalf
                 );
         }
@@ -77,140 +77,76 @@ library Pietrzak_VDF {
                 return false;
             } else {
                 if (!output.calculated) return true;
-                //else if (output.x_prime != proofList[i + 1].x) return false;
-                else if (!equal(output.x_prime, proofList[i + 1].x)) return false;
-                //else if (output.y_prime != proofList[i + 1].y) return false;
-                else if (!equal(output.y_prime, proofList[i + 1].y)) return false;
+                else if (!output.x_prime.eq(proofList[i + 1].x)) return false;
+                else if (!output.y_prime.eq(proofList[i + 1].y)) return false;
                 else if (output.T_half != proofList[i + 1].T) return false;
             }
         }
         return true;
     }
 
-    // /**
-    //  *
-    //  * @param a base value
-    //  * @param b exponent value
-    //  * @return result of a^b mod N
-    //  * @notice powerModN function
-    //  * @notice calculate a^b mod N
-    //  * @notice O(log b) complexity
-    //  */
-    // function powerModN(uint256 a, uint256 b, uint256 n) internal pure returns (uint256) {
-    //     uint256 result = 1;
-    //     while (b > 0) {
-    //         if (b & 1 == 1) {
-    //             result = mulmod(result, a, n);
-    //         }
-    //         a = mulmod(a, a, n);
-    //         b = b / 2;
-    //     }
-    //     return result;
-    // }
-
-    function powerModN(
-        bytes memory _base,
-        bytes memory _exp,
-        bytes memory _mod
-    ) public view returns (bytes memory result) {
-        assembly {
-            let bl := mload(_base)
-            let el := mload(_exp)
-            let ml := mload(_mod)
-            let fmp := mload(0x40)
-
-            mstore(fmp, bl)
-            mstore(add(fmp, 32), el)
-            mstore(add(fmp, 64), ml)
-            if iszero(staticcall(gas(), 0x4, add(_base, 32), bl, add(fmp, 96), bl)) {
-                revert(0, 0)
+    /**
+     * OpenZeppelin Contracts (last updated v4.9.0) (utils/Strings.sol)
+     * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+     */
+    function toString(uint256 value) internal pure returns (string memory) {
+        unchecked {
+            uint256 length = log10(value) + 1;
+            string memory buffer = new string(length);
+            uint256 ptr;
+            /// @solidity memory-safe-assembly
+            assembly {
+                ptr := add(buffer, add(32, length))
             }
-            let offset := add(96, bl)
-            if iszero(staticcall(gas(), 0x4, add(_exp, 32), el, add(fmp, offset), el)) {
-                revert(0, 0)
-            }
-            offset := add(offset, el)
-            if iszero(staticcall(gas(), 0x4, add(_mod, 32), ml, add(fmp, offset), ml)) {
-                revert(0, 0)
-            }
-            offset := add(offset, ml)
-            if iszero(staticcall(gas(), 0x5, fmp, offset, add(fmp, 96), ml)) {
-                revert(0, 0)
-            }
-            // point to the location of the return value (length, bits)
-            //result := add(fmp, 64)
-            let length := ml
-            let ptr := add(fmp, 96)
-            /// the following code removes any leading words containing all zeros in the result.
-            for {
-
-            } eq(eq(length, 32), 0) {
-
-            } {
-                switch eq(mload(ptr), 0)
-                case 1 {
-                    ptr := add(ptr, 32)
+            while (true) {
+                ptr--;
+                /// @solidity memory-safe-assembly
+                assembly {
+                    mstore8(ptr, byte(mod(value, 10), _SYMBOLS))
                 }
-                default {
-                    break
-                }
-                length := sub(length, 32)
+                value /= 10;
+                if (value == 0) break;
             }
-            result := sub(ptr, 32)
-            mstore(result, length)
-
-            mstore(0x40, add(add(fmp, 96), ml))
+            return buffer;
         }
     }
 
-    // function mul(bytes memory _a, bytes memory _b) internal view returns (bytes memory) {
-    //     bytes memory addResult = _add(_a, _b);
-    // }
-
-    function init(bytes memory val) internal view returns (BigNumber memory result) {
-        return BigNumber(val, val.length * 8);
-    }
-
-    function equal(bytes memory _preBytes, bytes memory _postBytes) internal pure returns (bool) {
-        bool success = true;
-
-        assembly {
-            let length := mload(_preBytes)
-
-            // if lengths don't match the arrays are not equal
-            switch eq(length, mload(_postBytes))
-            case 1 {
-                // cb is a circuit breaker in the for loop since there's
-                //  no said feature for inline assembly loops
-                // cb = 1 - don't breaker
-                // cb = 0 - break
-                let cb := 1
-
-                let mc := add(_preBytes, 0x20)
-                let end := add(mc, length)
-
-                for {
-                    let cc := add(_postBytes, 0x20)
-                    // the next line is the loop condition:
-                    // while(uint256(mc < end) + cb == 2)
-                } eq(add(lt(mc, end), cb), 2) {
-                    mc := add(mc, 0x20)
-                    cc := add(cc, 0x20)
-                } {
-                    // if any of these checks fails then arrays are not equal
-                    if iszero(eq(mload(mc), mload(cc))) {
-                        // unsuccess:
-                        success := 0
-                        cb := 0
-                    }
-                }
+    /**
+     * openzeppelin-contracts/contracts/utils/math/Math.sol
+     * @dev Return the log in base 10, rounded down, of a positive value.
+     * Returns 0 if given 0.
+     */
+    function log10(uint256 value) internal pure returns (uint256) {
+        uint256 result = 0;
+        unchecked {
+            if (value >= 10 ** 64) {
+                value /= 10 ** 64;
+                result += 64;
             }
-            default {
-                // unsuccess:
-                success := 0
+            if (value >= 10 ** 32) {
+                value /= 10 ** 32;
+                result += 32;
+            }
+            if (value >= 10 ** 16) {
+                value /= 10 ** 16;
+                result += 16;
+            }
+            if (value >= 10 ** 8) {
+                value /= 10 ** 8;
+                result += 8;
+            }
+            if (value >= 10 ** 4) {
+                value /= 10 ** 4;
+                result += 4;
+            }
+            if (value >= 10 ** 2) {
+                value /= 10 ** 2;
+                result += 2;
+            }
+            if (value >= 10 ** 1) {
+                result += 1;
             }
         }
-
-        return success;
+        return result;
     }
 }
