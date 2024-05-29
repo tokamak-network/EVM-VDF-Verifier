712cebd4be6a353264b29a291352be38d1fce6e3
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-05-29 16:36:59 +0900

    create deposit and request for Titan scripts

---
+++ scripts/ForTitan/2-fiveOperatorsDepositForTitan.ts
@@ -0,0 +1,50 @@
+// Copyright 2024 justin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { deployments, ethers, network } from "hardhat"
+
+async function fiveOperatorsDeposit() {
+    const chainId: number = network.config.chainId as number
+    const signers = await ethers.getSigners()
+    const crrrngCoordinatorAddress = (await deployments.get("CRRNGCoordinatorPoFForTitan")).address
+    console.log("CRRNGCoordinatorPoFForTitan address:", crrrngCoordinatorAddress)
+    const crrngCoordinatorContract = await ethers.getContractAt(
+        "CRRNGCoordinatorPoFForTitan",
+        crrrngCoordinatorAddress,
+    )
+    const minimumDepositAmount = await crrngCoordinatorContract.getMinimumDepositAmount()
+    for (let i: number = 0; i < 5; i++) {
+        const depositedAmount = await crrngCoordinatorContract.getDepositAmount(signers[i].address)
+        if (depositedAmount < BigInt(minimumDepositAmount)) {
+            const tx = await crrngCoordinatorContract.connect(signers[i]).operatorDeposit({
+                value: BigInt(minimumDepositAmount) - depositedAmount,
+            })
+            const receipt = await tx.wait()
+        }
+        const depositedAmountAfter = await crrngCoordinatorContract.getDepositAmount(
+            signers[i].address,
+        )
+        console.log(
+            `Operator ${i} deposited amount: ${ethers.formatEther(
+                depositedAmountAfter.toString(),
+            )} ETH`,
+        )
+    }
+}
+
+fiveOperatorsDeposit()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
