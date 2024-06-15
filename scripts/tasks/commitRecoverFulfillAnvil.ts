e6d49b050d070f1acd3a8d1f562dbd5ef8e08b09
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-06-15 16:59:51 +0900

    update scripts
    #22

---
+++ scripts/tasks/commitRecoverFulfillAnvil.ts
@@ -0,0 +1,78 @@
+// Copyright 2024 justin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { time } from "@nomicfoundation/hardhat-network-helpers"
+import { BigNumberish, BytesLike } from "ethers"
+import { task } from "hardhat/config"
+interface BigNumber {
+    val: BytesLike
+    bitlen: number
+}
+function getLength(value: number): number {
+    let length: number = 32
+    while (length < value) length += 32
+    return length
+}
+export const getBitLenth2 = (num: string): BigNumberish => {
+    return BigInt(num).toString(2).length
+}
+
+task("commitRecoverFulfillAnvil", "Operator commits For Titan")
+    .addParam("round", "The round")
+    .setAction(async ({ round }, { deployments, ethers, getNamedAccounts }) => {
+        const { deployer } = await getNamedAccounts()
+        console.log("EOA address:", deployer)
+        const crrrngCoordinatorAddress = (await deployments.get("CRRNGCoordinatorPoFForTitan"))
+            .address
+        console.log("CRRNGCoordinatorPoFForTitan address:", crrrngCoordinatorAddress)
+        const crrngCoordinatorContract = await ethers.getContractAt(
+            "CRRNGCoordinatorPoFForTitan",
+            crrrngCoordinatorAddress,
+        )
+        for (let i: number = 0; i < 3; i++) {
+            const commitCount = (await crrngCoordinatorContract.getValuesAtRound(round))
+                .commitCounts
+            const signer = (await ethers.getSigners())[Number(commitCount)]
+            let rand = crypto.getRandomValues(new Uint8Array(2048 / 8))
+            const bytesHex = "0x" + rand.reduce((o, v) => o + ("00" + v.toString(16)).slice(-2), "")
+
+            const commitData = {
+                val: ethers.toBeHex(bytesHex, getLength(ethers.dataLength(bytesHex))),
+                bitlen: getBitLenth2(bytesHex),
+            }
+            console.log(commitData)
+
+            const tx = await crrngCoordinatorContract.connect(signer).commit(round, commitData)
+            await tx.wait()
+            console.log(`Operator ${commitCount} committed successfully`)
+        }
+        // ** recover
+        let rand = crypto.getRandomValues(new Uint8Array(2048 / 8))
+        const bytesHex = "0x" + rand.reduce((o, v) => o + ("00" + v.toString(16)).slice(-2), "")
+        const recover = {
+            val: ethers.toBeHex(bytesHex, getLength(ethers.dataLength(ethers.toBeHex(bytesHex)))),
+            bitlen: getBitLenth2(ethers.toBeHex(bytesHex)),
+        }
+        await time.increase(121)
+        console.log("time increased by 121 seconds")
+        let tx = await crrngCoordinatorContract.recover(round, recover)
+        await tx.wait()
+        console.log("Recovered successfully")
+
+        const disputePeriod = 180
+        await time.increase(BigInt(disputePeriod) + 1n)
+        console.log("time increased by 181 seconds")
+        tx = await crrngCoordinatorContract.fulfillRandomness(round)
+        await tx.wait()
+        console.log("Fulfilled successfully")
+    })
