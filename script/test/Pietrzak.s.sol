d55e6d113d0f6e4eaf030abe53470dc46273cc38
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-07-25 13:41:28 +0900

    create Pietrzak script to test on real network

---
+++ script/test/Pietrzak.s.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script, console2} from "forge-std/Script.sol";
+import "../../src/test/MinimalPietrzak.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract DeployAndTestPietrzak is Script {
+    struct JsonBigNumber {
+        uint256 bitlen;
+        bytes val;
+    }
+
+    function decodeBigNumber(
+        bytes memory jsonBytes
+    ) public pure returns (BigNumber memory) {
+        JsonBigNumber memory xJsonBigNumber = abi.decode(
+            jsonBytes,
+            (JsonBigNumber)
+        );
+        BigNumber memory x = BigNumber(
+            xJsonBigNumber.val,
+            xJsonBigNumber.bitlen
+        );
+        return x;
+    }
+
+    function returnParsed(
+        uint256 bits,
+        uint256 i,
+        uint256 tau
+    )
+        public
+        view
+        returns (
+            BigNumber[] memory v,
+            BigNumber memory x,
+            BigNumber memory y,
+            BigNumber memory n,
+            uint256 delta,
+            uint256 T
+        )
+    {
+        string memory root = vm.projectRoot();
+        string memory path = string(
+            abi.encodePacked(
+                root,
+                "/test/shared/pietrzakTestCases/",
+                Strings.toString(bits),
+                "/T",
+                Strings.toString(tau),
+                "/",
+                Strings.toString(i),
+                ".json"
+            )
+        );
+        string memory json = vm.readFile(path);
+        x = decodeBigNumber(vm.parseJson(json, ".recoveryProofs[0].x"));
+        y = decodeBigNumber(vm.parseJson(json, ".recoveryProofs[0].y"));
+        delta = 9;
+        uint256 proofLength = tau - delta;
+        v = new BigNumber[](proofLength);
+        for (uint256 j; j < proofLength; j++) {
+            v[j] = (
+                decodeBigNumber(
+                    vm.parseJson(
+                        json,
+                        string.concat(
+                            ".recoveryProofs[",
+                            Strings.toString(j),
+                            "].v"
+                        )
+                    )
+                )
+            );
+        }
+        n = decodeBigNumber(vm.parseJson(json, ".n"));
+        T = uint256(bytes32((vm.parseJson(json, ".T"))));
+    }
+
+    function run() external {
+        BigNumber[] memory v;
+        BigNumber memory x;
+        BigNumber memory y;
+        BigNumber memory n;
+        uint256 delta;
+        uint256 T;
+        (v, x, y, n, delta, T) = returnParsed(2048, 1, 20);
+        MinimalPietrzakExternal minimalPietrzak204822 = MinimalPietrzakExternal(
+            DevOpsTools.get_most_recent_deployment(
+                "MinimalPietrzakExternal",
+                block.chainid
+            )
+        );
+        vm.startBroadcast();
+        //MinimalPietrzakExternal minimalPietrzak204822 = new MinimalPietrzakExternal();
+        bool result = minimalPietrzak204822.verifyPietrzak(
+            v,
+            x,
+            y,
+            n,
+            delta,
+            T
+        );
+        vm.stopBroadcast();
+        console2.log("result: ", result);
+    }
+}
