0985b8e20437af8ac29e5829dd12088d9bd17051
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-08-01 11:43:23 +0900

    decode bigNumber from json in solidity

---
+++ test/shared/DecodeJsonBigNumber.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {BigNumber} from "../../src/libraries/BigNumbers.sol";
+
+contract DecodeJsonBigNumber {
+    struct JsonBigNumber {
+        uint256 bitlen;
+        bytes val;
+    }
+
+    function decodeBigNumber(
+        bytes memory jsonBytes
+    ) public pure returns (BigNumber memory) {
+        JsonBigNumber memory xJsonBigNumber = abi.decode(
+            jsonBytes,
+            (JsonBigNumber)
+        );
+        BigNumber memory x = BigNumber(
+            xJsonBigNumber.val,
+            xJsonBigNumber.bitlen
+        );
+        return x;
+    }
+}
